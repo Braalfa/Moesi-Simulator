6f9e4beaf47135d68d39d0fb98dfb63b00772cbd
Brayan Alfaro G <alfabrayan12@gmail.com>
2022-03-17 22:50:18 -0600

    New times

--- Proyecto/Timing/timing.py
+++ Proyecto/Timing/timing.py
@@ -2,13 +2,13 @@ import time
 
 
 class Timing:
-    def __init__(self, base_timing: float = 1):
+    def __init__(self, base_timing: float = 0.5):
         self.base_timing = base_timing
         self.execute_timing = self.base_timing
         self.memory_timing = self.base_timing*5
         self.cache_timing = self.base_timing*2
-        self.max_bus_data_timing = self.memory_timing*2
-        self.await_sharers_timing = self.base_timing*3
+        self.max_bus_data_timing = self.memory_timing*3
+        self.await_sharers_timing = self.memory_timing*3
         self.infinite_timing = 1000
 
     def cache_wait(self):
